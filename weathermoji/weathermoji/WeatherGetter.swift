23c31b454a39a85e152d6b6c882ee98ea0e5203e
Andrew Carvajal <andrewcarvajal1@gmail.com>
2017-01-04 10:16:21 -0500

    Added WeatherGetter class

---
+++ weathermoji/weathermoji/WeatherGetter.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherGetter.swift
+//  weathermoji
+//
+//  Created by Andrew Carvajal on 1/4/17.
+//  Copyright © 2017 Yuge. All rights reserved.
+//
+
+import Foundation
+
+class WeatherGetter {
+    
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapAPIKey = "10abd2dcd8626a4d75165599ff7c8625"
+    
+    func getWeather(city: String) {
+        
+        // This is a pretty simple networking task, so the shared session will do.
+        let session = URLSession.shared
+        
+        let weatherRequestURL = NSURL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)")!
+        
+        // The data task retrieves the data.
+        let dataTask = session.dataTask(with: weatherRequestURL as URL) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                // Case 1: Error
+                // We got some kind of error while trying to get data from the server.
+                print("Error:\n\(error)")
+            }
+            else {
+                // Case 2: Success
+                // We got a response from the server!
+                print("Data:\n\(data!)")
+            }
+        }
+        
+        // The data task is set up...launch it!
+        dataTask.resume()
+    }
+    
+}
