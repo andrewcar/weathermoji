beda4bfc94494f03a488649a54ac1911d0ffc205
Andrew Carvajal <andrewcarvajal1@gmail.com>
2017-01-04 10:02:43 -0500

    Initial commit

---
+++ weathermoji/weathermoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weathermoji
+//
+//  Created by Andrew Carvajal on 1/4/17.
+//  Copyright © 2017 Yuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
